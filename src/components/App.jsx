ef4b0d45c3e171aa26d78f69a04a2b75616777a7
Oleksii Bohoslavskyi <oleks.bohos@gmail.com>
2023-12-21 23:49:33 +0200

    add Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from './Profile/Profile.jsx';
+
+const App = () => (
+  <div className="app-wrapper">
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+  </div>
+);
+
+export { App };
